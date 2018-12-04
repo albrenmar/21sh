bcbc7feac8656c22a4757dc34f38c8b19f9b3108
Marc <joserubiomarc@gmail.com>
2018-12-04 01:48:40 +0100

    MJOSE :: TEST :: PIPES :: Pipe correcto

--- pipes/main.c
+++ pipes/main.c
@@ -6,7 +6,7 @@
 /*   By: mjose <mjose@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/27 00:59:52 by mjose             #+#    #+#             */
-/*   Updated: 2018/11/27 04:22:50 by mjose            ###   ########.fr       */
+/*   Updated: 2018/11/28 00:29:21 by mjose            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,48 +14,60 @@
 
 #define SIZE 512
 
+#define LEER		0
+#define ESCRIBIR	1
 
 int		main(int ac, char **av)
 {
 	pid_t	pid;
-	int		a[2];
-	int		b[2];
-	int		readb;
-	int		desc_fitch;
-	char	buf[SIZE];
+	int		descrf[2];
+//	int		b[2];
+//	int		readb;
+//	int		desc_fitch;
+//	char	buf[SIZE];
 
 	if (ac < 3)
 	{
 		printf("Formto: %s fichero comando [opciones].\n", av[0]);
 		exit(1);
 	}
-	printf("Ejemplo de redireccion.\n");
-	desc_fitch = open(av[1], O_CREAT|O_TRUNC|O_WRONLY, 0);
-	dup2(desc_fitch, 1);
-	close(desc_fitch);
-	pipe(a);
-	pipe(b);
+//	printf("Ejemplo de redireccion.\n");
+//	desc_fitch = open(av[1], O_CREAT|O_TRUNC|O_WRONLY, 0);
+//	dup2(desc_fitch, 1);
+	pipe(descrf);
+//	close(desc_fitch);
+//	pipe(a);
+//	pipe(b);
 	if ((pid = fork()) == 0)
 	{
-		close(a[1]);
-		close(b[0]);
-		execvp(av[2], &av[2]);
-		while ((readb = read(a[0], buf, SIZE)) > 0)
-			write(1, buf, readb);
-		ft_strcpy(buf, "Soy tu hijo hablandote por la otra tuberia\n");
-		write(b[1], buf, ft_strlen(buf));
-		close(b[1]);
+		close(descrf[LEER]);
+//		close(b[0]);
+		dup2(descrf[ESCRIBIR], 1);
+		close(descrf[ESCRIBIR]);
+//		execvp(av[2], &av[2]);
+		execlp(av[1], av[1], NULL);
+		// while ((readb = read(a[0], buf, SIZE)) > 0)
+		// 	write(1, buf, readb);
+		// ft_strcpy(buf, "Soy tu hijo hablandote por la otra tuberia\n");
+		// write(b[1], buf, ft_strlen(buf));
+		// close(b[1]);
+		perror(av[0]);
 	}
 	else
 	{
-		close(a[0]);
-		close(b[1]);
-		ft_strcpy(buf, "Soy tu padre hablandote por una tuberia\n");
-		write(a[1], buf, ft_strlen(buf));
-		close(a[1]);
-		while ((readb = read(b[0], buf, SIZE)) > 0)
-			write(1, buf, readb);
-		close(b[0]);
+		close(descrf[ESCRIBIR]);
+		dup2(descrf[LEER], 0);
+		close(descrf[LEER]);
+		execlp(av[2], av[2], av[3], NULL);
+		perror(0);
+	// 	close(a[0]);
+	// 	close(b[1]);
+	// 	ft_strcpy(buf, "Soy tu padre hablandote por una tuberia\n");
+	// 	write(a[1], buf, ft_strlen(buf));
+	// 	close(a[1]);
+	// 	while ((readb = read(b[0], buf, SIZE)) > 0)
+	// 		write(1, buf, readb);
+	// 	close(b[0]);
 	}
 	waitpid(pid, NULL, 0);
 	exit(0);
