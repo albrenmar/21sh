346ff3c08792b6f69057c5397e8ed03e6d25d0a2
Marc <joserubiomarc@gmail.com>
2019-01-18 06:03:39 +0100

    MJOSE :: TEST :: JOB_CONTROL :: Traduciendo codigo

---
+++ job_control/job_control.h
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   job_control.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjose <mjose@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/18 02:04:26 by mjose             #+#    #+#             */
+/*   Updated: 2019/01/18 05:51:36 by mjose            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef JOB_CONTROL_H
+# define JOB_CONTROL_H
+
+# include "../minishell/includes/minishell.h"
+# include <termios.h>
+
+typedef struct		s_process
+{
+	char				**argv;
+	pid_t				pid;
+	char				completed;
+	char				stopped;
+	int					status;
+	struct s_process	*next;
+}					t_process;
+
+typedef struct		s_job
+{
+	char				*command;
+	t_process			*first_process;
+	pid_t				pgid;
+	char				notified;
+	struct termios		tmodes;
+	int					stdin;
+	int					stdout;
+	int					stderr;
+	struct s_job		*next;
+}					t_job;
+
+t_job				*g_first_job = NULL;
+pid_t				g_shell_pgid;
+struct termios		g_shell_tmodes;
+int					g_shell_terminal;
+int					g_shell_is_interactive;
+
+t_job				*find_job (pid_t pgid);
+int					job_is_stopped(t_job *job_list);
+int					job_is_completed(t_job *job_list);
+void				init_shell(void);
+void				launch_process(t_process *process_list, pid_t pgid, int infile, int outfile, int errfile, int foreground);
+void	launch_job(t_job *job_list, int foreground)
+
+#endif
--- job_control/main.c
+++ job_control/main.c
@@ -6,101 +6,172 @@
 /*   By: mjose <mjose@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/17 00:56:21 by mjose             #+#    #+#             */
-/*   Updated: 2019/01/17 05:45:20 by mjose            ###   ########.fr       */
+/*   Updated: 2019/01/18 05:51:37 by mjose            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../minishell/includes/minishell.h"
+#include "job_control.h"
 
-typedef struct	s_lproc
+t_job	*find_job(pid_t pgid)
 {
-	int				num_proc;
-	int				pid;
-	int				status;
-	struct s_lproc	*next;
-}				t_lproc;
+	t_job	*job_list;
 
-	t_lproc		*tmp_proc;
-	int			id = 0;
-
-void		kill_child(int signum)
-{
-	//kill(tmp_proc->pid, SIGINT);
-	signum = SIGINT;
-	kill(tmp_proc->pid, signum);
-	wait(&tmp_proc->status);
-}
-
-void	stop_child(int signum)
-{
-//	kill(tmp_proc->pid, SIGSTOP);
-	signum = SIGSTOP;
-	kill(tmp_proc->pid, signum);
-	wait(&tmp_proc->status);
-	if (WIFSTOPPED(tmp_proc->status))
+	job_list = g_first_job;
+	while (job_list)
 	{
-		ft_putchar('[');
-		ft_putnbr(tmp_proc->num_proc);
-		ft_putchar(']');
-		ft_putendl("\t+ suspended\t");
+		if (job_list->pgid == pgid)
+			return (job_list);
+		job_list = job_list->next;
 	}
+	return (NULL);
 }
 
-t_lproc		*init_proc(void)
+int		job_is_stopped(t_job *job_list)
 {
-	t_lproc		*proc;
+	t_process	*process_list;
 
-	proc = (t_lproc *)malloc(sizeof(t_lproc));
-	proc->num_proc = 0;
-	proc->pid = 0;
-	proc->status = 0;
-	proc->next = NULL;
-	return (proc);
-}
-
-int	crt_child(t_lproc **list_proc)
-{
-	t_lproc			*new_proc;
-
-	new_proc = init_proc();
-	new_proc->num_proc = ++id;
-	tmp_proc = new_proc;
-//	if (fork() == 0)
-//	{
-		ft_putendl("Presiona CTRL+C o CTRL+Z");
-		if ((tmp_proc->pid = fork()) == 0)
-			while (1);
-		else
-		{
-			signal(SIGINT, &kill_child);
-			signal(SIGTSTP, &stop_child);
-			wait(&tmp_proc->status);
-//			exit(0);
-		}
-//	}
-//	else
-	tmp_proc->next = ft_memalloc(sizeof(t_lproc));
-	if (WIFSTOPPED(tmp_proc->status))
+	process_list = job_list->first_process;
+	while (process_list)
 	{
-		tmp_proc = tmp_proc->next;
-		list_proc = &tmp_proc;
-		return (0);
+		if (!process_list->completed && !process_list->stopped)
+			return (0);
+		process_list = process_list->next;
 	}
-	tmp_proc = tmp_proc->next;
-	list_proc = &tmp_proc;
 	return (1);
 }
 
-int		main(void)
+int		job_is_completed(t_job *job_list)
 {
-	t_lproc			*list_proc;
+	t_process	*process_list;
 
-	signal(SIGTSTP, SIG_IGN);
-	while (1)
+	process_list = job_list->first_process;
+	while (process_list)
 	{
-		if (crt_child(&list_proc))
-			ft_putendl("Creo nuevo hijo.");
-		else
-			break ;
+		if (!process_list->completed)
+			return (0);
+		process_list = process_list->next;
 	}
+	return (1);
+}
+
+void	init_shell(void)
+{
+	g_shell_terminal = STDIN_FILENO;
+	g_shell_is_interactive = isatty(g_shell_terminal);	/*****INTERDIT isatty*****/
+
+	if (g_shell_is_interactive)
+	{
+		while (tcgetpgrp(g_shell_terminal) != (g_shell_pgid = getpgrp())) /*****INTERDIT tcgetpgrp*****/
+			kill(-g_shell_pgid, SIGTTIN);
+		signal(SIGINT, SIG_IGN);
+		signal(SIGQUIT, SIG_IGN);
+		signal(SIGTSTP, SIG_IGN);
+		signal(SIGTTIN, SIG_IGN);
+		signal(SIGTTOU, SIG_IGN);
+		signal(SIGCHLD, SIG_IGN);
+		g_shell_pgid = getpid();
+		if (setpgid(g_shell_pgid, g_shell_pgid) < 0)
+		{
+			perror("No se pudo poner el shell en su propio grupo de procesos.");
+			exit(1);
+		}
+		tcsetpgrp(g_shell_terminal, g_shell_pgid); /*****INTERDIT tcsetpgrp*****/
+		tcgetattr(g_shell_terminal, &g_shell_tmodes); /*****INTERDIT tcgetattr*****/
+	}
+}
+
+void	launch_process(t_process *process_list, pid_t pgid, int infile, int outfile, int errfile, int foreground)
+{
+	pid_t		pid;
+
+	if (g_shell_is_interactive)
+	{
+		pid = getpid();
+		if (pgid == 0)
+			pgid = pid;
+		setpgid(pid, pgid);
+		if (foreground)
+			tcsetpgrp(g_shell_terminal, pgid);
+		signal(SIGINT, SIG_DFL);
+		signal(SIGQUIT, SIG_DFL);
+		signal(SIGTSTP, SIG_DFL);
+		signal(SIGTTIN, SIG_DFL);
+		signal(SIGTTOU, SIG_DFL);
+		signal(SIGCHLD, SIG_DFL);
+	}
+	if (infile != STDIN_FILENO)
+	{
+		dup2(infile, STDIN_FILENO);
+		close(infile);
+	}
+	if (outfile != STDOUT_FILENO)
+	{
+		dup2(outfile, STDOUT_FILENO);
+		close(outfile);
+	}
+	if (errfile != STDERR_FILENO)
+	{
+		dup2(errfile, STDERR_FILENO);
+		close(errfile);
+	}
+	execvp(process_list->argv[0], process_list->argv);  /*****INTERDIT execvp*****/
+	perror("execvp");
+	exit(1);
+}
+
+void	launch_job(t_job *job_list, int foreground)
+{
+	t_process	*process_list;
+	pid_t		pid;
+	int			mypipe[2];
+	int			infile;
+	int			outfile;
+
+	infile = job_list->stdin;
+	process_list = job_list->first_process;
+	while (process_list)
+	{
+		if (process_list->next)
+		{
+			if (pipe(mypipe) < 0)
+			{
+				perror("pipe");
+				exit(1);
+			}
+			outfile = mypipe[1];
+		}
+		else
+			outfile = job_list->stdout;
+		pid = fork();
+		if (pid == 0)
+			launch_process(process_list, job_list->pgid, infile, outfile, job_list->stderr, foreground);
+		else if (pid < 0)
+		{
+			perror("fork");
+			exit(1);
+		}
+		else
+		{
+			process_list->pid = pid;
+			if (g_shell_is_interactive)
+			{
+				if (job_list->pgid)
+					job->pgid = pid;
+				setpgid(pid, job_list->pgid);
+			}
+		}
+		if (infile != j->stdin)
+			close(infile);
+		if (outfile != job_list->stdout)
+			close(outfile);
+		infile = mypipe[0];
+		process_list = process_list->next;
+	}
+	format_job_info(job_list, "launched");
+	if (!g_shell_is_interactive)
+		wait_for_job(job_list);
+	else if (foreground)
+		put_job_in_foreground(job_list, 0);
+	else
+		put_job_in_background(job_list, 0);
 }
