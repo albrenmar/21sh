b9d429ed649bc97a66ca3624f3ab1aa1c2cb8791
Marc Jose Rubio <joserubiomarc@gmail.com>
2019-02-18 11:44:59 +0100

    MJOSE :: TEST :: EXPANSIONS :: LINUX :: Soluciono problema al anadir una nueva variable SET

--- srcs/expansions/environ_set.c
+++ srcs/expansions/environ_set.c
@@ -6,7 +6,7 @@
 /*   By: mjose <mjose@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/17 08:40:37 by mjose             #+#    #+#             */
-/*   Updated: 2019/02/17 10:37:57 by mjose            ###   ########.fr       */
+/*   Updated: 2019/02/18 11:38:32 by mjose            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ t_env_set	*new_envset(char *varname, char *varvalue)
 	t_env_set	*env_lst;
 	t_keyval	*buf;
 
+	env_lst = NULL;
+	buf = NULL;
 	buf = (t_keyval *)malloc(sizeof(t_keyval));
 	env_lst = (t_env_set *)malloc(sizeof(t_env_set));
 	buf->key = varname;
@@ -32,6 +34,7 @@ void	add_to_env_set(char *varname, char *varvalue)
 {
 	t_env_set	*env_list;
 	t_env_set	*first;
+	t_env_set	*new_envvar;
 
 	if (g_tracking.mysh->setenv_lst == NULL)
 		g_tracking.mysh->setenv_lst = new_envset(varname, varvalue);
@@ -39,8 +42,10 @@ void	add_to_env_set(char *varname, char *varvalue)
 	{
 		env_list = g_tracking.mysh->setenv_lst;
 		first = g_tracking.mysh->setenv_lst;
-		while (env_list)
+		while (env_list && env_list->next)
 			env_list = env_list->next;
-		env_list = new_envset(varname, varvalue);
+		new_envvar = new_envset(varname, varvalue);
+		env_list->next = new_envvar;
+		g_tracking.mysh->setenv_lst = first;
 	}
 }
