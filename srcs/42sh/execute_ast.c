b33be937e9ad91d12b7f2f92e901e04c0fb2fb2d
Alexandre <alsomvil@student.42.fr>
2019-02-19 09:33:27 +0100

    Execution en cours de travaux

--- srcs/42sh/execute_ast.c
+++ srcs/42sh/execute_ast.c
@@ -6,7 +6,7 @@
 /*   By: alsomvil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/05 00:59:46 by alsomvil          #+#    #+#             */
-/*   Updated: 2019/02/19 06:59:09 by alsomvil         ###   ########.fr       */
+/*   Updated: 2019/02/19 09:31:32 by alsomvil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,44 +17,61 @@ int		exec_command(t_last *list_cmd)
 {
 	pipe(descrf);
 	pipe(descrf_two);
-	int		close_fd;
-	t_last	*temp_list;
-
+	t_last	*begin;
+	t_last	*temp;
+	t_last	*temp_op;
+	int		STDIN;
+	int		STDOUT;
+	int		STDERR;
 	//init_fd(NULL);
-	while (list_cmd)
+	/*while (list_cmd)
 	{
 		printf("CMD = %s TYPE = %d\n", list_cmd->name, list_cmd->type);
 		list_cmd = list_cmd->next;
-	}
-	/*while (list_cmd)
+	}*/
+	begin = list_cmd;
+	while (list_cmd)
 	{
-		if (list_cmd->type == CMD)
+		temp = list_cmd;
+		while (list_cmd && list_cmd->type != OP)
 		{
-			if (next_is_pipe(list_cmd))
-			{
-				pipe(descrf_two);
-				execute_pipe();
-			}
-			else if (next_is_reddir(list_cmd))
-			{
-				if ((temp_list = its_fd_reddir(list_cmd)))
-					create_fich(temp_list, list_cmd);
-			}
+			//if (list_cmd->type == DESCR)
+			//	set_fd(list_cmd->name);
 			list_cmd = list_cmd->next;
 		}
-		if (list_cmd->type == DESCR)
+		if (list_cmd)
+			temp_op = list_cmd;
+		list_cmd = temp;
+		if (its_pipe(temp_op))
 		{
-			init_fd(list_cmd);
+			descrf[0] = descrf_two[0];
+			descrf[1] = descrf_two[1];
+			pipe(descrf_two);
+			//execute_pipe();
 		}
-	}*/
+		else if (its_reddir(temp_op))
+		{
+			while (temp_op->type != FICH)
+				temp_op = temp_op->next;
+			STDOUT = open(temp_op->name, O_CREAT | O_TRUNC | O_RDWR, 0644);
+		}
+		else if (its_fd_reddir(temp_op))
+		{
+			//VERIF FD
+		}
+		list_cmd = temp;
+		while (list_cmd->type != CMD)
+			list_cmd = list_cmd->next;
+		printf("J'EXECUTE %s\n", list_cmd->name);
+		execve(list_cmd->name, NULL, NULL);
+		exit (0);
+		//execute_two(list_cmd->name);
+		
+	}
 	/*while (ORDER)
 	{
 		while (ORDER && ORDER->sym && ORDER->sym[0] == '|')
 		{
-	y1		descrf[0] = descrf_two[0];
-			descrf[1] = descrf_two[1];
-			pipe(descrf_two);
-			execute_pipe();
 			ORDER = ORDER->next;
 			if (!ORDER->next)
 			{
