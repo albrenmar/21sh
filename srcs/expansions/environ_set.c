ef36b1cc70a190c50a5b36faddbf027db3d90385
Marc <joserubiomarc@gmail.com>
2019-02-22 07:19:38 +0100

    MJOSE :: TEST :: EXPANSIONS :: NORM :: environ_set.c y set.c :: a la norma

--- srcs/expansions/environ_set.c
+++ srcs/expansions/environ_set.c
@@ -6,7 +6,7 @@
 /*   By: mjose <mjose@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/17 08:40:37 by mjose             #+#    #+#             */
-/*   Updated: 2019/02/19 06:07:16 by mjose            ###   ########.fr       */
+/*   Updated: 2019/02/22 07:16:21 by mjose            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void		replace_env_set_str(char *varname, char *varvalue)
 	{
 		set_env = g_tracking.mysh->setenv_lst;
 		first = g_tracking.mysh->setenv_lst;
-		while (set_env)
+		while (set_env && set_env->param)
 		{
 			if (ft_strequ(varname, set_env->param->key))
 			{
@@ -36,10 +36,7 @@ void		replace_env_set_str(char *varname, char *varvalue)
 			if (set_env->next)
 				set_env = set_env->next;
 			else
-			{
 				add_to_env_set(varname, varvalue);
-				break ;
-			}
 		}
 		g_tracking.mysh->setenv_lst = first;
 	}
@@ -52,31 +49,38 @@ t_env_set	*new_envset(char *varname, char *varvalue)
 
 	env_lst = NULL;
 	buf = NULL;
-	buf = (t_keyval *)malloc(sizeof(t_keyval));
 	env_lst = (t_env_set *)malloc(sizeof(t_env_set));
-	buf->key = ft_strdup(varname);
-	buf->value = ft_strdup(varvalue);
-	env_lst->param = buf;
+	if (varname)
+	{
+		buf = (t_keyval *)malloc(sizeof(t_keyval));
+		buf->key = ft_strdup(varname);
+		buf->value = ft_strdup(varvalue);
+		env_lst->param = buf;
+	}
 	env_lst->next = NULL;
-
 	return (env_lst);
 }
 
-void	add_to_env_set(char *varname, char *varvalue)
+void		add_to_env_set(char *varname, char *varvalue)
 {
 	t_env_set	*env_list;
 	t_env_set	*first;
 	t_env_set	*new_envvar;
 
 	if (g_tracking.mysh->setenv_lst == NULL)
+	{
 		g_tracking.mysh->setenv_lst = new_envset(varname, varvalue);
+		g_tracking.mysh->setenv_lst->next = new_envset(NULL, NULL);
+	}
 	else
 	{
 		env_list = g_tracking.mysh->setenv_lst;
 		first = g_tracking.mysh->setenv_lst;
-		while (env_list && env_list->next)
+		while (env_list && env_list->param)
 			env_list = env_list->next;
+		free(&new_envvar);
 		new_envvar = new_envset(varname, varvalue);
+		new_envvar->next = new_envset(NULL, NULL);
 		env_list->next = new_envvar;
 		g_tracking.mysh->setenv_lst = first;
 	}
