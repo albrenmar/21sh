efa8d64da7652848ff8400d5691d59e1d52b52ca
Marc <joserubiomarc@gmail.com>
2019-02-22 05:44:50 +0100

    MJOSE :: TEST :: EXPANSIONS :: NORM :: environ.c :: a la norma

--- srcs/expansions/environ.c
+++ srcs/expansions/environ.c
@@ -6,18 +6,36 @@
 /*   By: mjose <mjose@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/16 05:36:36 by mjose             #+#    #+#             */
-/*   Updated: 2019/02/19 05:34:27 by mjose            ###   ########.fr       */
+/*   Updated: 2019/02/22 05:42:23 by mjose            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "expansions.h"
 #include "sh42.h"
 
+int		have_setname(char *var)
+{
+	t_env_set	*set_env;
+	t_keyval	*buf;
+
+	set_env = g_tracking.mysh->setenv_lst;
+	while (set_env)
+	{
+		buf = set_env->param;
+		if (ft_strcmp(var, buf->key) == 0)
+			return (2);
+		if (set_env->next)
+			set_env = set_env->next;
+		else
+			return (0);
+	}
+	return (0);
+}
+
 int		have_envname(char *var)
 {
 	t_list		*env;
 	t_keyval	*buf;
-	t_env_set	*set_env;
 
 	if (g_tracking.mysh->env != NULL)
 	{
@@ -35,16 +53,5 @@ int		have_envname(char *var)
 	}
 	if (g_tracking.mysh->setenv_lst == NULL)
 		return (0);
-	set_env = g_tracking.mysh->setenv_lst;
-	while (set_env)
-	{
-		buf = set_env->param;
-		if (ft_strcmp(var, buf->key) == 0)
-			return (2);
-		if (set_env->next)
-			set_env = set_env->next;
-		else
-			return (0);
-	}
-	return (0);
+	return (have_setname(var));
 }
