d6b412f41f4d79cb2468e997ee5fc61452163702
Abraham Guimbao <abram.2197@gmail.com>
2019-03-07 07:06:31 +0100

    bug jobs repare

--- srcs/jobs/signal_handlers.c
+++ srcs/jobs/signal_handlers.c
@@ -6,7 +6,7 @@
 /*   By: abe <abe@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/15 12:52:33 by alsomvil          #+#    #+#             */
-/*   Updated: 2019/03/07 06:50:44 by abe              ###   ########.fr       */
+/*   Updated: 2019/03/07 07:05:44 by abe              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,69 +40,121 @@ void		sig_winch_handler(int signo)
 	}
 }
 
-void					set_process_signal_handlers(void)
+void		set_process_signal_handlers(void)
 {
-	// signal(SIGSEGV, SIG_IGN);
-	// signal(SIGABRT, SIG_IGN);
-	// signal(SIGKILL, SIG_DFL);
-	// signal(SIGSTOP, SIG_DFL);
-	struct sigaction	psa;
-    
-	psa.sa_handler = SIG_DFL;
-    sigaction(SIGINT, &psa, NULL);
-	psa.sa_handler = SIG_DFL;
-    sigaction(SIGQUIT, &psa, NULL);
-	psa.sa_handler = SIG_DFL;
-    sigaction(SIGTSTP, &psa, NULL);
-	psa.sa_handler = SIG_DFL;
-    sigaction(SIGCONT, &psa, NULL);
-	psa.sa_handler = SIG_DFL;
-    sigaction(SIGTTIN, &psa, NULL);
-	psa.sa_handler = SIG_DFL;
-    sigaction(SIGTTOU, &psa, NULL);
-	psa.sa_handler = SIG_DFL;
-    sigaction(SIGCHLD, &psa, NULL);
+	// struct sigaction signalAction;
+    // signalAction.sa_handler = SIG_DFL;
+    // sigemptyset(&signalAction.sa_mask);
+    // signalAction.sa_flags = 0;      // SA_RESTART;
+    // sigaction(SIGTSTP, &signalAction, NULL);
 
-	// signal(SIGINT, SIG_DFL);
-	// signal(SIGQUIT, SIG_DFL);
-	// signal(SIGTSTP, SIG_DFL);
-	// signal(SIGCONT, SIG_DFL);
-	// signal(SIGTTOU, SIG_DFL);
-	// signal(SIGTTIN, SIG_DFL);
-	// signal(SIGCHLD, SIG_DFL);
+	// // struct sigaction signalAction;
+    // signalAction.sa_handler = SIG_DFL;
+    // sigemptyset(&signalAction.sa_mask);
+    // signalAction.sa_flags = 0;      // SA_RESTART;
+    // sigaction(SIGSTOP, &signalAction, NULL);
+
+
+	// // struct sigaction signalAction;
+    // signalAction.sa_handler = SIG_DFL;
+    // sigemptyset(&signalAction.sa_mask);
+    // signalAction.sa_flags = 0;      // SA_RESTART;
+    // sigaction(SIGCONT, &signalAction, NULL);
+
+	// signal(SIGKILL, SIG_DFL);
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+	signal(SIGTSTP, SIG_DFL);
+	signal(SIGCONT, SIG_DFL);
+	// signal(SIGSTOP, SIG_DFL);
+	signal(SIGTTIN, SIG_DFL);
+	signal(SIGTTOU, SIG_DFL);
+	signal(SIGTTIN, SIG_DFL);
+	signal(SIGTTOU, SIG_DFL);
+	signal(SIGCHLD, SIG_DFL);
 }
 
-void					set_shell_signal_handlers(void)
+void		set_shell_signal_handlers(void)
 {
 	// signal(SIGSEGV, SIG_IGN);
-	// signal(SIGABRT, SIG_IGN);
 	// signal(SIGKILL, SIG_IGN);
+	// signal(SIGABRT, SIG_IGN);
+
+	signal(SIGINT, sig_int_handler);
+	signal(SIGWINCH, sig_winch_handler);
+	signal(SIGQUIT, SIG_IGN);
+	signal(SIGTSTP, SIG_IGN);
+	signal(SIGCONT, SIG_IGN);
 	// signal(SIGSTOP, SIG_IGN);
-	struct sigaction	psa;
-
-    psa.sa_handler = sig_int_handler;
-    sigaction(SIGINT, &psa, NULL);
-	psa.sa_handler = sig_winch_handler;
-    sigaction(SIGWINCH, &psa, NULL);
-	psa.sa_handler = SIG_IGN;
-    sigaction(SIGQUIT, &psa, NULL);
-	psa.sa_handler = SIG_IGN;
-    sigaction(SIGTSTP, &psa, NULL);
-	psa.sa_handler = SIG_IGN;
-    sigaction(SIGCONT, &psa, NULL);
-	psa.sa_handler = SIG_IGN;
-    sigaction(SIGTTIN, &psa, NULL);
-	psa.sa_handler = SIG_IGN;
-    sigaction(SIGTTOU, &psa, NULL);
-	psa.sa_handler = SIG_IGN;
-    sigaction(SIGCHLD, &psa, NULL);
-
-	// signal(SIGWINCH, sig_winch_handler);
-	// signal(SIGINT, sig_int_handler);
-	// signal(SIGQUIT, SIG_IGN);
-	// signal(SIGTSTP, SIG_IGN);
-	// signal(SIGCONT, SIG_IGN);
-	// signal(SIGTTOU, SIG_IGN);
-	// signal(SIGTTIN, SIG_IGN);
+	signal(SIGTTIN, SIG_IGN);
+	signal(SIGTTOU, SIG_IGN);
+	signal(SIGCHLD, SIG_DFL);
 	// signal(SIGCHLD, SIG_IGN);
 }
+
+// void					set_process_signal_handlers(void)
+// {
+// 	// signal(SIGSEGV, SIG_IGN);
+// 	// signal(SIGABRT, SIG_IGN);
+// 	// signal(SIGKILL, SIG_DFL);
+// 	// signal(SIGSTOP, SIG_DFL);
+// 	struct sigaction	psa;
+    
+// 	psa.sa_handler = SIG_DFL;
+//     sigaction(SIGINT, &psa, NULL);
+// 	psa.sa_handler = SIG_DFL;
+//     sigaction(SIGQUIT, &psa, NULL);
+// 	psa.sa_handler = SIG_DFL;
+//     sigaction(SIGTSTP, &psa, NULL);
+// 	psa.sa_handler = SIG_DFL;
+//     sigaction(SIGCONT, &psa, NULL);
+// 	psa.sa_handler = SIG_DFL;
+//     sigaction(SIGTTIN, &psa, NULL);
+// 	psa.sa_handler = SIG_DFL;
+//     sigaction(SIGTTOU, &psa, NULL);
+// 	psa.sa_handler = SIG_DFL;
+//     sigaction(SIGCHLD, &psa, NULL);
+
+// 	// signal(SIGINT, SIG_DFL);
+// 	// signal(SIGQUIT, SIG_DFL);
+// 	// signal(SIGTSTP, SIG_DFL);
+// 	// signal(SIGCONT, SIG_DFL);
+// 	// signal(SIGTTOU, SIG_DFL);
+// 	// signal(SIGTTIN, SIG_DFL);
+// 	// signal(SIGCHLD, SIG_DFL);
+// }
+
+// void					set_shell_signal_handlers(void)
+// {
+// 	// signal(SIGSEGV, SIG_IGN);
+// 	// signal(SIGABRT, SIG_IGN);
+// 	// signal(SIGKILL, SIG_IGN);
+// 	// signal(SIGSTOP, SIG_IGN);
+// 	struct sigaction	psa;
+
+//     psa.sa_handler = sig_int_handler;
+//     sigaction(SIGINT, &psa, NULL);
+// 	psa.sa_handler = sig_winch_handler;
+//     sigaction(SIGWINCH, &psa, NULL);
+// 	psa.sa_handler = SIG_IGN;
+//     sigaction(SIGQUIT, &psa, NULL);
+// 	psa.sa_handler = SIG_IGN;
+//     sigaction(SIGTSTP, &psa, NULL);
+// 	psa.sa_handler = SIG_IGN;
+//     sigaction(SIGCONT, &psa, NULL);
+// 	psa.sa_handler = SIG_IGN;
+//     sigaction(SIGTTIN, &psa, NULL);
+// 	psa.sa_handler = SIG_IGN;
+//     sigaction(SIGTTOU, &psa, NULL);
+// 	psa.sa_handler = SIG_IGN;
+//     sigaction(SIGCHLD, &psa, NULL);
+
+// 	// signal(SIGWINCH, sig_winch_handler);
+// 	// signal(SIGINT, sig_int_handler);
+// 	// signal(SIGQUIT, SIG_IGN);
+// 	// signal(SIGTSTP, SIG_IGN);
+// 	// signal(SIGCONT, SIG_IGN);
+// 	// signal(SIGTTOU, SIG_IGN);
+// 	// signal(SIGTTIN, SIG_IGN);
+// 	// signal(SIGCHLD, SIG_IGN);
+// }
