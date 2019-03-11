52b79827f2d8f75500a4b1512581f3838fe618fb
Marc <joserubiomarc@gmail.com>
2019-03-11 11:22:24 +0100

    MJOSE :: MAC42 :: EXPANSIONS :: Solucionando bugs (##, ##*fsdfds y ##fdfd*) generado correctamente

---
+++ srcs/expansions/analyzer.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   analyzer.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjose <mjose@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/10 18:01:55 by mjose             #+#    #+#             */
+/*   Updated: 2019/03/11 11:14:56 by mjose            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "expansions.h"
+#include "sh42.h"
+
+void	init_analyzer(t_analyzer *to_analy, char **str, t_expand *expand)
+{
+	to_analy->orig_str = *str;
+	to_analy->orig_len = ft_strlen(to_analy->orig_str);
+	to_analy->varname = get_varname(expand);
+	to_analy->vnme_len = ft_strlen(to_analy->varname);
+	to_analy->varvalue = get_env_string(to_analy->varname);
+	to_analy->vvlu_len = ft_strlen(to_analy->varvalue);
+	to_analy->wildcard = get_value(expand);
+	to_analy->wlcd_len = ft_strlen(to_analy->wildcard);
+	if (to_analy->wildcard[to_analy->wlcd_len - 1] == '*'
+			|| to_analy->wildcard[0] == '*')
+	{
+		to_analy->asterisk = 1;
+		to_analy->wlcd_len--;
+		if (to_analy->wildcard[0] == '*')
+			to_analy->start_astrsk = 1;
+		else
+			to_analy->start_astrsk = 0;
+		if (to_analy->wildcard[to_analy->wlcd_len] == '*')
+			to_analy->end_astrsk = 1;
+		else
+			to_analy->end_astrsk = 0;
+	}
+	else
+		to_analy->asterisk = 0;
+}
