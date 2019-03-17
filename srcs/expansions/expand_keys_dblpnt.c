49505525c1ff1c2d1c004c5970f02b3cb123242f
Marc <joserubiomarc@gmail.com>
2019-03-17 23:17:12 +0100

    MJOSE :: MAC42 :: EXPANSIONS :: NORM :: expand_keys_dblpnt.c :: exp_key_plus :: A la norma

--- srcs/expansions/expand_keys_dblpnt.c
+++ srcs/expansions/expand_keys_dblpnt.c
@@ -6,7 +6,7 @@
 /*   By: mjose <mjose@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 06:41:37 by mjose             #+#    #+#             */
-/*   Updated: 2019/03/17 22:55:02 by mjose            ###   ########.fr       */
+/*   Updated: 2019/03/17 23:14:11 by mjose            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,46 +18,22 @@ void	exp_key_plus(char **str, t_expand *expand)
 	t_analyzer	to_analy;
 
 	init_analyzer(&to_analy, str, expand);
-/*	char	*str1;
-	char	*str2;
-	char	*value1;
-	char	*value2;
-
-	str1 = NULL;
-	str2 = NULL;
-	str1 = get_varname(expand);
-	str2 = get_value(expand);
-	value1 = get_env_string(str1);
-	if (!value1)
-		value1 = get_parm_string(str1);
-	value2 = str2;
-*/	ft_strdel(str);
+	ft_strdel(str);
 	if (!to_analy.vnme_len)
 	{
-		print_exp_error_dpoints(to_analy.varname, to_analy.wildcard);
+		print_exp_error_dpoints(to_analy.varname, to_analy.wildcard, '+');
 		return ;
 	}
-//	ft_strdel(&str1);
-//	if (value1)
 	if (to_analy.varvalue)
 	{
-//		*str = value2;
 		if (to_analy.wildcard)
 			*str = to_analy.wildcard;
 		else
 			*str = ft_strdup(" ");
-//		ft_strdel(&value1);
-//		if (!value2)
-//			*str = ft_strdup("");
 	}
 	else
 		*str = ft_strdup(" ");
-/*	else
-	{
-		ft_strdel(&value2);
-		*str = NULL;
-	}
-*/}
+}
 
 void	exp_key_inter(char **str, t_expand *expand)
 {
