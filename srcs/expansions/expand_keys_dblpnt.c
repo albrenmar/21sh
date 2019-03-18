abb4853e4ba197c0dcde4d32cf33a08887bfcb4a
Marc <joserubiomarc@gmail.com>
2019-03-18 21:44:21 +0100

    MJOSE :: MAC42 :: EXPANSIONS :: NORM :: expand_keys_dblpnt.c :: exp_key_equal :: A la norma

--- srcs/expansions/expand_keys_dblpnt.c
+++ srcs/expansions/expand_keys_dblpnt.c
@@ -6,7 +6,7 @@
 /*   By: mjose <mjose@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 06:41:37 by mjose             #+#    #+#             */
-/*   Updated: 2019/03/18 21:25:18 by mjose            ###   ########.fr       */
+/*   Updated: 2019/03/18 21:37:25 by mjose            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	exp_key_plus(char **str, t_expand *expand)
 		print_exp_error_dpoints(to_analy.varname, to_analy.wildcard, '+');
 		return ;
 	}
-	if (to_analy.varvalue)
+	if (to_analy.varvalue[0])
 	{
 		if (to_analy.wildcard)
 			*str = to_analy.wildcard;
@@ -54,38 +54,17 @@ void	exp_key_inter(char **str, t_expand *expand)
 void	exp_key_equal(char **str, t_expand *expand)
 {
 	t_analyzer	to_analy;
+	int			env_or_set;
 
 	init_analyzer(&to_analy, str, expand);
-/*	char	*str1;
-	char	*str2;
-	char	*value1;
-	char	*value2;
-*/	int		env_or_set;
-/*
-	str1 = NULL;
-	str2 = NULL;
-	str1 = get_varname(expand);
-	str2 = get_value(expand);
-	value1 = get_env_string(str1);
-	if (!value1)
-		value1 = get_parm_string(str1);
-	value2 = str2;
-*/	ft_strdel(str);
-//	env_or_set = have_envname(str1);
+	ft_strdel(str);
 	env_or_set = have_envname(to_analy.varname);
-//	if (value1 && !ft_strequ(value1, ""))
 	if (to_analy.varvalue && to_analy.varvalue[0])
-	{
 		*str = to_analy.varvalue;
-//		*str = value1;
-	}
 	else if (env_or_set == 1)
 		replace_env_str(to_analy.varname, to_analy.wildcard);
-//		replace_env_str(str1, value2);
 	else if (env_or_set == 2 || (!env_or_set && to_analy.vnme_len))
 		replace_env_set_str(to_analy.varname, to_analy.wildcard);
-//		replace_env_set_str(str1, value2);
-//	ft_strdel(&str1);
 	else
 		print_exp_error_dpoints(to_analy.varname, to_analy.wildcard, '=');
 	if (!*str)
@@ -95,7 +74,6 @@ void	exp_key_equal(char **str, t_expand *expand)
 		else
 			*str = to_analy.varvalue;
 	}
-//		*str = value2;
 }
 
 void	exp_key_less(char **str, t_expand *expand)
