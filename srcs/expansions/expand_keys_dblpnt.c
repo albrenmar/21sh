0fa4b0521bb98367b95e4a1b4c45a5bfe3d44d34
Marc <joserubiomarc@gmail.com>
2019-03-28 02:19:05 +0100

    MJOSE :: MAC42 :: EXPANSIONS :: LEAKS :: Leaks ADVANCED ${HOME:?} controlados

--- srcs/expansions/expand_keys_dblpnt.c
+++ srcs/expansions/expand_keys_dblpnt.c
@@ -6,7 +6,7 @@
 /*   By: mjose <mjose@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 06:41:37 by mjose             #+#    #+#             */
-/*   Updated: 2019/03/28 02:07:14 by mjose            ###   ########.fr       */
+/*   Updated: 2019/03/28 02:17:54 by mjose            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,12 +46,19 @@ void	exp_key_inter(char **str, t_expand *expand)
 	if (to_analy.varvalue)
 	{
 		ft_strdel(str);
-		*str = to_analy.varvalue;
+		*str = ft_strdup(to_analy.varvalue);
 	}
 	else if (to_analy.varname && to_analy.varname[0])
+	{
 		print_exp_error_eq(to_analy.varname, to_analy.wildcard);
+		*str = ft_strdup(" ");
+	}
 	else
+	{
 		print_exp_error_dpoints(to_analy.varname, to_analy.wildcard, '?');
+		*str = ft_strdup(" ");
+	}
+	end_analyzer(to_analy);
 }
 
 void	exp_key_equal(char **str, t_expand *expand)
