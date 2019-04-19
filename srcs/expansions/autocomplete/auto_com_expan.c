e82eb7e72961f2a637720a89948a0e78a26c7625
Marc <joserubiomarc@gmail.com>
2019-04-19 07:54:36 +0200

    MJOSE :: MASTER :: Petit fail...

--- srcs/expansions/autocomplete/auto_com_expan.c
+++ srcs/expansions/autocomplete/auto_com_expan.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   auto_com_expan.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bsiche <bsiche@student.42.fr>              +#+  +:+       +#+        */
+/*   By: mjose <mjose@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 03:46:47 by mjose             #+#    #+#             */
-/*   Updated: 2019/04/11 04:25:53 by bsiche           ###   ########.fr       */
+/*   Updated: 2019/04/19 07:52:25 by mjose            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_exp_complete(char *arg)
 	{
 		tmp = ft_strdup(arg);
 		ft_free(arg);
-		expand_transformer(&tmp);
+		expand_transformer(&tmp, 0);
 		return (tmp);
 	}
 	return (NULL);
