ad3582c9a3206594922f2d2c3e9af9c2cd0ea616
Marc <joserubiomarc@gmail.com>
2019-04-21 23:17:43 +0200

    MJOSE :: EXPANSIONS :: ERRORS :: Generando errores de forma correcta

--- srcs/expansions/expand_error.c
+++ srcs/expansions/expand_error.c
@@ -6,7 +6,7 @@
 /*   By: mjose <mjose@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/03 06:17:07 by mjose             #+#    #+#             */
-/*   Updated: 2019/04/20 04:30:02 by mjose            ###   ########.fr       */
+/*   Updated: 2019/04/21 22:48:18 by mjose            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 void	print_exp_error_dpoints(char *varname, char *value, int sign, char **str)
 {
-	if (g_tracking.mysh->in_ast)
+	if (g_tracking.mysh->in_ast && !g_tracking.mysh->err_expend_printed)
 	{
 		ft_putstr_fd(SHELL_NAME, 2);
 		ft_putstr_fd(": ${", 2);
@@ -25,6 +25,7 @@ void	print_exp_error_dpoints(char *varname, char *value, int sign, char **str)
 		ft_putstr_fd(value, 2);
 		ft_putendl_fd("}: bad substitution", 2);
 		g_tracking.mysh->err_expend = 0;
+		g_tracking.mysh->err_expend_printed = 1;
 	}
 	else
 	{
