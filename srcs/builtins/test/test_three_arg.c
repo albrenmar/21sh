2dee395e36f5e26912a9f910263d89e5c5acb295
Abraham <abram.2197@gmail.com>
2019-04-30 22:52:44 +0200

    fix test -gt

--- srcs/builtins/test/test_three_arg.c
+++ srcs/builtins/test/test_three_arg.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   test_three_arg.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mjose <mjose@student.42.fr>                +#+  +:+       +#+        */
+/*   By: abguimba <abguimba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/20 05:31:34 by bsiche            #+#    #+#             */
-/*   Updated: 2019/04/18 02:10:31 by mjose            ###   ########.fr       */
+/*   Updated: 2019/04/30 22:50:27 by abguimba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,11 +46,8 @@ long		get_nbr(char *str)
 	return (ft_atoi(str));
 }
 
-int			math_test(char **argv)
+int			math_test(char **argv, long i, long j)
 {
-	long	i;
-	long	j;
-
 	i = get_nbr(argv[0]);
 	j = get_nbr(argv[2]);
 	if (i > 2147483647 || j > 2147483647)
@@ -64,6 +61,9 @@ int			math_test(char **argv)
 	if (ft_strcmp(argv[1], "-ge") == 0)
 		if (i >= j)
 			return (0);
+	if (ft_strcmp(argv[1], "-gt") == 0)
+		if (i > j)
+			return (0);
 	if (ft_strcmp(argv[1], "-it") == 0)
 		if (i < j)
 			return (0);
@@ -76,14 +76,18 @@ int			math_test(char **argv)
 int			three_arg(char **argv)
 {
 	int		i;
+	long	j;
+	long	k;
 
+	j = 0;
+	k = 0;
 	i = 1;
 	if (ft_strcmp(argv[1], "=") == 0 || ft_strcmp(argv[1], "!=") == 0)
 		return (string_test(argv));
 	if (ft_strcmp(argv[1], "-eq") == 0 || ft_strcmp(argv[1], "-ne") == 0
 	|| ft_strcmp(argv[1], "-ge") == 0 || ft_strcmp(argv[1], "-it") == 0
-	|| ft_strcmp(argv[1], "-le") == 0)
-		return (math_test(argv));
+	|| ft_strcmp(argv[1], "-le") == 0 || ft_strcmp(argv[1], "-gt") == 0)
+		return (math_test(argv, j, k));
 	ft_putendl_fd("too many arguments", 2);
 	return (i);
 }
