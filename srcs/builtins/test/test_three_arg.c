e5f2f79f35728ca853f773d93ef5a4b61ecd8b72
Abraham <abram.2197@gmail.com>
2019-04-30 23:02:54 +0200

    fix test -lt

--- srcs/builtins/test/test_three_arg.c
+++ srcs/builtins/test/test_three_arg.c
@@ -6,7 +6,7 @@
 /*   By: abguimba <abguimba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/20 05:31:34 by bsiche            #+#    #+#             */
-/*   Updated: 2019/04/30 22:50:27 by abguimba         ###   ########.fr       */
+/*   Updated: 2019/04/30 23:02:00 by abguimba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,8 +48,6 @@ long		get_nbr(char *str)
 
 int			math_test(char **argv, long i, long j)
 {
-	i = get_nbr(argv[0]);
-	j = get_nbr(argv[2]);
 	if (i > 2147483647 || j > 2147483647)
 		return (1);
 	if (ft_strcmp(argv[1], "-eq") == 0)
@@ -70,24 +68,24 @@ int			math_test(char **argv, long i, long j)
 	if (ft_strcmp(argv[1], "-le") == 0)
 		if (i <= j)
 			return (0);
+	if (ft_strcmp(argv[1], "-lt") == 0)
+		if (i < j)
+			return (0);
 	return (1);
 }
 
 int			three_arg(char **argv)
 {
 	int		i;
-	long	j;
-	long	k;
 
-	j = 0;
-	k = 0;
 	i = 1;
 	if (ft_strcmp(argv[1], "=") == 0 || ft_strcmp(argv[1], "!=") == 0)
 		return (string_test(argv));
 	if (ft_strcmp(argv[1], "-eq") == 0 || ft_strcmp(argv[1], "-ne") == 0
 	|| ft_strcmp(argv[1], "-ge") == 0 || ft_strcmp(argv[1], "-it") == 0
-	|| ft_strcmp(argv[1], "-le") == 0 || ft_strcmp(argv[1], "-gt") == 0)
-		return (math_test(argv, j, k));
+	|| ft_strcmp(argv[1], "-le") == 0 || ft_strcmp(argv[1], "-gt") == 0
+	|| ft_strcmp(argv[1], "-lt") == 0)
+		return (math_test(argv, get_nbr(argv[0]), get_nbr(argv[2])));
 	ft_putendl_fd("too many arguments", 2);
 	return (i);
 }
