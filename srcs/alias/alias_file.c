b71c94a8f765c1d45a7649210b6a28722e18eff2
Abraham <abram.2197@gmail.com>
2019-05-02 10:45:10 +0200

    bug alias

--- srcs/alias/alias_file.c
+++ srcs/alias/alias_file.c
@@ -6,7 +6,7 @@
 /*   By: abguimba <abguimba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/15 22:29:58 by bsiche            #+#    #+#             */
-/*   Updated: 2019/05/02 05:42:05 by abguimba         ###   ########.fr       */
+/*   Updated: 2019/05/02 10:37:48 by abguimba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,13 +39,16 @@ int		init_alias(int fd, t_lstcontainer *alias_lst)
 		ft_putstr_fd("Failed to create or load ~/42shrc, ", 2);
 		ft_putstr_fd("please check your read/write permissions", 2);
 	}
-	alias_lst = lstcontainer_new();
-	while (get_next_line(fd, &alias) != 0)
+	else
 	{
-		lstcontainer_add(alias_lst, parse_alias(alias));
-		ft_free(alias);
+		alias_lst = lstcontainer_new();
+		while (get_next_line(fd, &alias) != 0)
+		{
+			lstcontainer_add(alias_lst, parse_alias(alias));
+			ft_free(alias);
+		}
+		close(fd);
+		g_tracking.mysh->alias_lst = alias_lst;
 	}
-	close(fd);
-	g_tracking.mysh->alias_lst = alias_lst;
 	return (0);
 }
