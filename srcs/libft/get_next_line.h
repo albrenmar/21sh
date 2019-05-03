72c2974f982c17ad263608c6127fdfb5a1ffb62e
Brendan Siche <B.siche@gmail.com>
2019-05-03 10:48:18 +0200

    gnl bufer

--- srcs/libft/get_next_line.h
+++ srcs/libft/get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: bsiche <bsiche@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/14 15:05:03 by bsiche            #+#    #+#             */
-/*   Updated: 2018/08/07 01:41:46 by bsiche           ###   ########.fr       */
+/*   Updated: 2019/05/03 10:13:16 by bsiche           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 6
+# define BUFF_SIZE 1024
 # include <stdlib.h>
 # include <unistd.h>
 # include "libft.h"
